590659b6dd97e9ffa2dd9a221f8ff6448fc1345a
konarjg <konarskikrzysztof1@gmail.com>
2022-01-19 11:23:15 +0100

    Fixed errors in syntax

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -1,29 +1,31 @@
-CREATE DATABASE warehouse;
+CREATE DATABASE IF NOT EXISTS warehouse;
 
-CREATE TABLE Products (
+USE warehouse;
+
+CREATE TABLE IF NOT EXISTS Products (
 	id int NOT NULL AUTO_INCREMENT,
 	name text,
 	description text,
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE Products_Instances (
+CREATE TABLE IF NOT EXISTS Products_Instances (
 	id int NOT NULL AUTO_INCREMENT,
 	locationID int NOT NULL,
 	productID int NOT NULL,
-	quantity int DEFAULT 0
+	quantity int DEFAULT 0,
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE Products_History (
+CREATE TABLE IF NOT EXISTS Products_History (
 	id int NOT NULL AUTO_INCREMENT,
 	productID int NOT NULL,
 	lastUpdate date NOT NULL,
-	quantity int DEFAULT 0
+	quantity int DEFAULT 0,
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE Locations(
+CREATE TABLE IF NOT EXISTS Locations(
 	id int NOT NULL AUTO_INCREMENT,
 	name text,
 	PRIMARY KEY(id)
