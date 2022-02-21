3b7591a148057ae6b969d68d4dce99ff16b4b764
Gabriel Malanowski <spgabilew5@gmail.com>
2022-02-21 18:55:43 +0000

    Added a Dataforemail structure with basic fields.

--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -6,6 +6,12 @@ class Core{
 public:
     Core();
 
+    struct Dataforemail {
+	std::string email_address_recipient;
+	std::string product_name;
+	std::string message;
+    };
+
     void DemandIndicator();
 
 };
