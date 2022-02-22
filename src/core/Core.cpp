d8802cf56561013250137c01e8e13d5702de184c
Gabriel Malanowski <spgabilew5@gmail.com>
2022-02-22 16:17:50 +0000

    Structures and function prototypes have been added.

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -4,6 +4,25 @@ namespace bakcyl::core {
 
 Core::Core()
 {
+
+	struct Dataforemail email_data;
+
+	unsigned short DemandIndicator(int quality, std::time_t date) {
+		return 0;
+	}
+
+	void GetDataforemailtoserver(struct Dataforemail* data) {
+		// data.email_address_recipient = ;
+		// data.product_name = ;
+		// data.message = ;
+	}
+
+	void SendDataforemailtoserver(struct Dataforemail* data) {
+		// data.email_address_recipient;
+                // data.product_name;
+                // data.message;
+	}
+
 }
 
 }
