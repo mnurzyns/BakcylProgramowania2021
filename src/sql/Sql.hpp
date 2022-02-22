4405cbaf6f602b75acb78a5d7b8f1b8f2c81818c
Krzysztof Konarski <konarskikrzysztof1@gmail.com>
2022-02-22 16:39:09 +0100

    Removed Product class from SQL folder and moved rest of the files from examples to main folder.

--- src/sql/Sql.hpp
+++ src/sql/Sql.hpp
@@ -1,10 +1,25 @@
 #pragma once
+#include <vector>
+#include <string>
 
-namespace bakcyl::sql {
+namespace bakcyl::sql 
+{
 
 class Sql {
 public:
     Sql();
+    //Products queries
+    std::vector<Product> selectAllProducts();
+    Product selectProduct(std::uint64_t productId);
+    std::vector<Product> selectProductsBeginningWith(std::string sequence);
+    std::vector<Product> selectProductsContaining(std::string sequence);
+    void insertProducts(std::vector<Product>& products);
+    void updateProducts(std::vector<int> ids, std::vector<Product>& newData);
+
+    //Instances queries
+    std::vector<ProductInstance> selectAllInstances();
+    void insertInstance(ProductInstance& instance);
+    void updateInstance(ProductInstance& newData);
 };
 
 }
