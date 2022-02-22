29661284238a0577493590e5578372e111a9c48e
Indoxer <indoxer.mk@gmail.com>
2022-02-22 17:56:52 +0100

    Add product instance class.

---
+++ src/common/productInstance.cpp
@@ -0,0 +1,47 @@
+#include "productInstance.hpp"
+
+namespace bakcyl::common
+{
+	//Getters
+	std::uint32_t ProductInstance::getId() const
+	{
+		return id;
+	}
+
+    std::string ProductInstance::getLocationId() const 
+    {
+        return locationId;
+    }
+
+    std::uint64_t ProductInstance::getProductId() const 
+    {
+        return productId;
+    }
+
+    std::uint32_t ProductInstance::getQuantity() const 
+    {
+        return quantity;
+    }
+
+    //Setters
+    void ProductInstance::setId(const std::uint32_t & value)
+    {
+        id = value;
+    }
+
+    void ProductInstance::setLocationId(const std::string & value)
+    {
+        locationId = value;
+    }
+
+    void ProductInstance::setProductId(const std::uint64_t & value)
+    {
+        productId = value;
+    }
+
+    void ProductInstance::setQuantity(const std::uint32_t & value)
+    {
+        quantity = value;
+    }
+
+}
---
+++ src/common/productInstance.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+namespace bakcyl::common {
+	class ProductInstance {
+	private:
+
+        std::uint32_t id;
+        std::string locationId;
+        std::uint64_t productId;
+        std::uint32_t quantity;
+        
+	public:
+		ProductInstance();
+		~ProductInstance();
+
+		//Getters
+		std::uint32_t getId() const;
+		std::string getLocationId() const;
+		std::uint64_t getProductId() const;
+        std::uint32_t getQuantity() const;
+
+
+		//Setters
+		void setId(const std::uint32_t & value);
+        void setLocationId(const std::string & value);
+        void setProductId(const std::uint64_t & value);
+        void setQuantity(const std::uint32_t & value);
+
+	};
+}
