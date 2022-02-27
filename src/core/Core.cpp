ab034854ed865f8626f7e582fddbb24a1aabc9ee
Gabriel Malanowski <spgabilew5@gmail.com>
2022-02-27 19:04:24 +0100

    Bugs fixed, passing variables by reference added.

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -4,26 +4,28 @@ namespace bakcyl::core {
 
 Core::Core()
 {
-
 	struct Dataforemail email_data;
 
-	unsigned short DemandIndicator(int quality, std::time_t date) {
-		return 0;
-	}
-
-	void GetDataforemailtoserver(struct Dataforemail* data) {
-		// data.email_address_recipient = ;
-		// data.product_name = ;
-		// data.message = "An order for the product <<product_name>> with the id <<product_id>> is needed.
-		//	 The product demand indicator is << demand_indicator >>" ;
-	}
-
-	void SendDataforemailtoserver(struct Dataforemail* data) {
-		// data.email_address_recipient;
-                // data.product_name;
-                // data.message;
-	}
-
 }
+
+    unsigned short DemandIndicator(const int &quality, std::time_t &date)
+    {
+        return 0;
+    }
+
+    void GetDataforemailtoserver(const struct Dataforemail &data)
+    {
+        // data.email_address_recipient = ;
+        // data.product_name = ;
+        // data.message = "An order for the product <<product_name>> with the id <<product_id>> is needed.
+        //	 The product demand indicator is << demand_indicator >>" ;
+    }
+
+    void SendDataforemailtoserver(const struct Dataforemail &data)
+    {
+        // data.email_address_recipient;
+        // data.product_name;
+        // data.message;
+    }
 
 }
--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <ctime>
 
 namespace bakcyl::core {
 
@@ -8,15 +9,15 @@ public:
     Core();
 
     struct Dataforemail {
-	std::string email_address_recipient;
-	std::string product_name;
-	std::string message;
-	short demand_indicator;
+            std::string email_address_recipient;
+            std::string product_name;
+            std::string message;
+            short demand_indicator;
     };
 
-    unsigned short DemandIndicator(int quality, std::time_t date);
-    void GetDataforemailtoserver(struct Dataforemail* data);
-    void SendDataforemailtoserver(struct Dataforemail* data);
+    unsigned short DemandIndicator(const int quality, std::time_t date);
+    void GetDataforemailtoserver(const struct Dataforemail &data);
+    void SendDataforemailtoserver(const struct Dataforemail &data);
 
 };
 
