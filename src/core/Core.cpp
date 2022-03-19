341ede260dd71c317ac509e3fe6b42b2684c99d1
Gabriel Malanowski <spgabilew5@gmail.com>
2022-03-19 18:45:15 +0100

    A method model was developed to enable the updating of product resources

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -5,5 +5,53 @@ namespace bakcyl::core {
 Core::Core()
 {
 }
-
+    std::uint32_t Core::getProductCurrentQuantity(const std::uint64_t &id) const
+    {
+        return currentQuantity;
+    }
+    std::uint32_t Core::getProductMinQuantity(const std::uint64_t &id) const
+    {
+        return Core::minQuantity;
+    }
+    std::uint32_t Core::getProductMaxQuantity(const std::uint64_t &id) const
+    {
+        return Core::maxQuantity;
+    }
+    std::uint8_t Core::changeProductQuantity(const std::uint64_t &id, const std::string &option, const std::uint32_t &quantity)
+    {
+        currentQuantity = getProductCurrentQuantity(id);
+        minQuantity = getProductMinQuantity(id);
+        maxQuantity = getProductMaxQuantity(id);
+        if(option == "increase")
+        {
+            if(currentQuantity+quantity > maxQuantity)
+            {
+                //If the condition above is true, it returns an error.
+                return 1;
+            }
+            else
+            {
+                //Returns control to PROXY for updating the product status in the database and returns 0 if everything was successful.
+                return 0;
+            }
+        }
+        else if(option == "decrease")
+        {
+            if(currentQuantity-quantity < minQuantity)
+            {
+                //If the condition above is true, it returns an error.
+                return 1;
+            }
+            else
+            {
+                //Returns control to PROXY for updating the product status in the database and returns 0 if everything was successful.
+                return 0;
+            }
+        }
+        else
+        {
+            //Returns 2 if the wrong "option" parameter was given.
+            return 2;
+        }
+    }
 }
--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -1,10 +1,21 @@
 #pragma once
+#include <string>
 
 namespace bakcyl::core {
 
 class Core{
 public:
     Core();
+
+    std::uint32_t currentQuantity;
+    std::uint32_t minQuantity;
+    std::uint32_t maxQuantity;
+
+    std::uint32_t getProductCurrentQuantity(const std::uint64_t &id) const;
+    std::uint32_t getProductMinQuantity(const std::uint64_t &id) const;
+    std::uint32_t getProductMaxQuantity(const std::uint64_t &id) const;
+    std::uint8_t changeProductQuantity(const std::uint64_t &id, const std::string &option, const std::uint32_t &quantity);
+
 };
 
 }
