2962501eacfe7f6dd1f522bb75146c1a9b0af67b
Gabriel Malanowski <spgabilew5@gmail.com>
2022-03-19 19:06:51 +0100

    Added tests

--- test/core/CoreTest.cpp
+++ test/core/CoreTest.cpp
@@ -9,5 +9,18 @@ TEST(CoreTest, itLives)
     Core();
 }
 
+TEST(CoreTest, changeProductQuantityShouldReturn2)
+{
+    Core core;
+    EXPECT_EQ(core.changeProductQuantity(0,"",1), 2);
+}
+
+TEST(CoreTest, changeProductQuantityShouldReturn1)
+{
+    Core core;
+    EXPECT_EQ(core.changeProductQuantity(0,"increase",1), 1);
+    EXPECT_EQ(core.changeProductQuantity(0,"decrease",1), 1);
+}
+
 }
 }
