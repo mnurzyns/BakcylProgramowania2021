8efbbfe07b06bbfd070b8fa12c67916aa1bb5e6f
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-03-21 17:54:04 +0100

    Improved readability of rowCount

--- src/gui/ProductModel.cpp
+++ src/gui/ProductModel.cpp
@@ -10,10 +10,12 @@ int ProductModel::rowCount(const QModelIndex &parent) const
     // For list models only the root node (an invalid parent) should return the list's size. For all
     // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
     if (parent.isValid())
+    {
         return 0;
+    }
 
-    // Hardcoded information about number of elements
-    return 10;
+    constexpr auto hardocedNumOfElements = 10u;
+    return hardocedNumOfElements;
 }
 
 bool ProductModel::hasChildren(const QModelIndex &parent) const
