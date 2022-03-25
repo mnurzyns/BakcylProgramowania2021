4f4be3510779f501f1989b61862481f588873dff
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-03-25 17:25:52 +0100

    Removed notify from QProperties, changed uint types to int (qml does not suppert them)

--- src/gui/ProductDataHandler.hpp
+++ src/gui/ProductDataHandler.hpp
@@ -5,25 +5,27 @@
 class ProductDataHandler : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(uint64_t productId MEMBER m_productId NOTIFY productDataChanged)
-    Q_PROPERTY(QString productName MEMBER m_productName NOTIFY productDataChanged)
-    Q_PROPERTY(QString productCategory MEMBER m_productCategory NOTIFY productDataChanged)
-    Q_PROPERTY(uint32_t productQuantity MEMBER m_productQuantity NOTIFY productDataChanged)
-    Q_PROPERTY(QString productLocation MEMBER m_productLocation NOTIFY productDataChanged)
-    Q_PROPERTY(QString productDescription MEMBER m_productDescription NOTIFY productDataChanged)
+    Q_PROPERTY(QString productId MEMBER m_productId)
+    Q_PROPERTY(QString productName MEMBER m_productName)
+    Q_PROPERTY(QString productCategory MEMBER m_productCategory)
+    Q_PROPERTY(QString productQuantity MEMBER m_productQuantity)
+    Q_PROPERTY(QString productLocation MEMBER m_productLocation)
+    Q_PROPERTY(QString productDescription MEMBER m_productDescription)
 
 public:
     explicit ProductDataHandler(QObject *parent = nullptr);
-    Q_INVOKABLE void getProductData(u_int64_t productId);
+    Q_INVOKABLE void getProductData(int productId);
 
 signals:
     void productDataChanged();
 
 private:
-    uint64_t m_productId;
+    uint64_t realProductId;
+
+    QString m_productId;
     QString m_productName;
     QString m_productCategory;
-    uint32_t m_productQuantity;
+    QString m_productQuantity;
     QString m_productLocation;
     QString m_productDescription;
 };
