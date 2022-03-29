0de681c49c3abfc6a4e9fb58ecc18142350a8cfd
Krzysztof Konarski <konarskikrzysztof1@gmail.com>
2022-03-29 16:47:52 +0200

    Created all Select queries for Products.

--- src/sql/Sql.cpp
+++ src/sql/Sql.cpp
@@ -90,4 +90,79 @@ namespace bakcyl::sql
         query.clear();
         return product;
     }
+
+    std::vector<Product> Sql::getProductsBeginningWith(const std::string& sequence) const
+    {
+        std::vector<Product> products;
+        
+        QSqlQuery query;
+        std::string syntax = "SELECT * FROM Products WHERE Name Like '" + sequence + "*'";
+        query.exec(syntax.c_str());
+
+        while (query.next()) 
+        {
+            const uint64_t id = query.value(0).toULongLong();
+            const std::string name = query.value(1).toString().toUtf8().constData();
+            const std::string description = query.value(2).toString().toUtf8().constData();
+            const std::string categories = query.value(3).toString().toUtf8().constData();
+            const uint32_t minQuantity = query.value(4).toInt();
+            const uint32_t maxQuantity = query.value(5).toInt();
+
+            Product product(id, name, description, categories, minQuantity, maxQuantity);
+            products.emplace_back(product);
+        }
+
+        query.clear();
+        return products;
+    }
+
+    std::vector<Product> Sql::getProductsContaining(const std::string& sequence) const
+    {
+        std::vector<Product> products;
+        
+        QSqlQuery query;
+        std::string syntax = "SELECT * FROM Products WHERE Name Like '*" + sequence + "*'";
+        query.exec(syntax.c_str());
+
+        while (query.next()) 
+        {
+            const uint64_t id = query.value(0).toULongLong();
+            const std::string name = query.value(1).toString().toUtf8().constData();
+            const std::string description = query.value(2).toString().toUtf8().constData();
+            const std::string categories = query.value(3).toString().toUtf8().constData();
+            const uint32_t minQuantity = query.value(4).toInt();
+            const uint32_t maxQuantity = query.value(5).toInt();
+
+            Product product(id, name, description, categories, minQuantity, maxQuantity);
+            products.emplace_back(product);
+        }
+
+        query.clear();
+        return products;
+    }
+
+    std::vector<Product> Sql::getProductsEndingWith(const std::string& sequence) const
+    {
+        std::vector<Product> products;
+        
+        QSqlQuery query;
+        std::string syntax = "SELECT * FROM Products WHERE Name Like '*" + sequence + "'";
+        query.exec(syntax.c_str());
+
+        while (query.next()) 
+        {
+            const uint64_t id = query.value(0).toULongLong();
+            const std::string name = query.value(1).toString().toUtf8().constData();
+            const std::string description = query.value(2).toString().toUtf8().constData();
+            const std::string categories = query.value(3).toString().toUtf8().constData();
+            const uint32_t minQuantity = query.value(4).toInt();
+            const uint32_t maxQuantity = query.value(5).toInt();
+
+            Product product(id, name, description, categories, minQuantity, maxQuantity);
+            products.emplace_back(product);
+        }
+
+        query.clear();
+        return products;
+    }
 };
