765fe95620c9f0dc5535bfd45be489793830d169
Gabriel Malanowski <spgabilew5@gmail.com>
2022-04-04 19:57:01 +0200

    The calculateDemandIndicator function is implemented

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -9,7 +9,12 @@ Core::Core()
 
     std::uint8_t Core::calculateDemandIndicator(const std::uint16_t quality, const std::time_t &date)
     {
-        return 0;
+        // The demand indicator returns in how many days the product will be out of stock
+        std::uint16_t demandFactor;
+        std::time_t nowTime;
+        time(&nowTime);
+        demandFactor = quality/(difftime(nowTime, date)/86400); //How much of the product will disappear in a day
+        return quality/demandFactor;
     }
 
     void Core::getEmailDataToServer(EmailData &data)
