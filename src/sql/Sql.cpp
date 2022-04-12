929daf3b6b2c30fad39087f08bdd0ee587d262f8
lilabyte <bicusintra@gmail.com>
2022-04-12 16:27:26 +0200

    Added INSERT and UPDATE queries for products

--- src/sql/Sql.cpp
+++ src/sql/Sql.cpp
@@ -175,4 +175,42 @@ namespace bakcyl::sql
         query.clear();
         return products;
     }
+
+    void Sql::insertProducts(const std::vector<Product>& products)
+    {
+        QSqlQuery query;
+        query.prepare("INSERT INTO products (name, description, categories, min_quantity, max_quantity) VALUES (:name, :description, :categories, :minQuantity, :maxQuantity)");
+
+        for (const Product& product : products)
+        {
+            query.bindValue(":name", QString(product.getName().c_str()));
+            query.bindValue(":description", QString(product.getDescription().c_str()));
+            query.bindValue(":categories", QString(product.getCategories().c_str()));
+            query.bindValue(":minQantity", product.getMinQuantity());
+            query.bindValue(":maxQuantity", product.getMaxQuantity());
+
+            query.exec();
+        }
+
+        query.clear();
+    }
+
+    void Sql::updateProducts(const std::vector<Product>& products)
+    {
+        QSqlQuery query;
+        query.prepare("UPDATE products SET name = :name, description = :description, categories = :categories, min_quantity = :minQuantity, max_quantity = :maxQuantity WHERE id = :id");
+
+        for (const Product& product : products)
+        {
+            query.bindValue(":name", QString(product.getName().c_str()));
+            query.bindValue(":description", QString(product.getDescription().c_str()));
+            query.bindValue(":categories", QString(product.getCategories().c_str()));
+            query.bindValue(":minQantity", product.getMinQuantity());
+            query.bindValue(":maxQuantity", product.getMaxQuantity());
+
+            query.exec();
+        }
+
+        query.clear();
+    }
 };
