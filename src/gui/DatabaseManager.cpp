3115f2f0034bc97fb8742c555d39ee8bda25b12c
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-04-16 18:48:02 +0200

    createNewProduct sends request to core

--- src/gui/DatabaseManager.cpp
+++ src/gui/DatabaseManager.cpp
@@ -8,5 +8,12 @@ DatabaseManager::DatabaseManager(QObject *parent)
 
 bool DatabaseManager::createNewProduct(int pId, QString pName, QString pCategories, QString pDescription)
 {
+    bakcyl::common::Product newProduct;
 
+    newProduct.setId(pId);
+    newProduct.setName(pName.toStdString());
+    newProduct.setCategories(pCategories.toStdString());
+    newProduct.setDescription(pDescription.toStdString());
+
+    bakcyl::core::Core::MethodResult queryResult = bakcyl::core::Core().createProduct(newProduct);
 }
