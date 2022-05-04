40cc1e78f182c31730aa54e6ef0bb70f30010115
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-05-04 10:31:17 +0200

    If query fails, fail message is exposed to qml

--- src/gui/DatabaseManager.cpp
+++ src/gui/DatabaseManager.cpp
@@ -17,11 +17,21 @@ bool DatabaseManager::createNewProduct(int pId, QString pName, QString pCategori
 
     bakcyl::core::Core::MethodResult queryResult = bakcyl::core::Core().createProduct(newProduct);
 
-    // TODO: Tell the user what happened
     if(queryResult == bakcyl::core::Core::MethodResult::SUCCESS)
     {
         return true;
     }
 
+    if(queryResult == bakcyl::core::Core::MethodResult::WRONG_PARAM)
+    {
+        m_failMessage = WRONG_PARAM_MESSAGE;
+    }
+
+    if(queryResult == bakcyl::core::Core::MethodResult::ERROR)
+    {
+        m_failMessage = ERROR_MESSAGE;
+    }
+
+    emit failMessageChanged();
     return false;
 }
